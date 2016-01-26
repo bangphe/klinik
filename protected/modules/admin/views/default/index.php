81c00a6f383e42409e5c54e882ac7266baefaca5
bangucen <Moh Husen>
2016-01-26 23:13:57 +0700

    update dashboard admin

--- protected/modules/admin/views/default/index.php
+++ protected/modules/admin/views/default/index.php
@@ -1,3 +1,9 @@
+<div class="note note-success">
+    <h3>Informasi Penting !!</h3>
+    <p>Obat <b> Oskadon </b> masa expired segera berakhir <b> 14 hari </b> lagi, silahkan isi stock dengan obat yang terbaru. Terima Kasih</p>
+    <p>Obat <b> Ultraflu </b> masa expired segera berakhir <b> 14 hari </b> lagi, silahkan isi stock dengan obat yang terbaru. Terima Kasih</p>
+    <p>Obat <b> Konidin </b> masa expired segera berakhir <b> 14 hari </b> lagi, silahkan isi stock dengan obat yang terbaru. Terima Kasih</p>
+</div>
 <div class="row">
     <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
         <div class="dashboard-stat blue">
@@ -22,10 +28,10 @@
             </div>
             <div class="details">
                 <div class="number">
-                    <span data-counter="counterup" data-value="12,5">12,5</span>M$ </div>
-                <div class="desc"> Total Profit </div>
+                    <span data-counter="counterup" data-value="12,5">Rp. 12.000.000</span></div>
+                <div class="desc"> Total Kredit Bulan ini </div>
             </div>
-            <a class="more" href="javascript:;"> View more
+            <a class="more" href="javascript:;"> Total Kredit Bulan ini
                 <i class="m-icon-swapright m-icon-white"></i>
             </a>
         </div>
@@ -62,3 +68,148 @@
         </div>
     </div>
 </div>
+<div class="col-md-6">
+    <!-- BEGIN CONDENSED TABLE PORTLET-->
+    <div class="portlet box red">
+        <div class="portlet-title">
+            <div class="caption">
+                <i class="fa fa-picture"></i>Obat Expired </div>
+                <div class="tools">
+                    <a href="javascript:;" class="collapse" data-original-title="" title=""> </a>
+                    <a href="#portlet-config" data-toggle="modal" class="config" data-original-title="" title=""> </a>
+                    <a href="javascript:;" class="reload" data-original-title="" title=""> </a>
+                    <a href="javascript:;" class="remove" data-original-title="" title=""> </a>
+                </div>
+            </div>
+            <div class="portlet-body">
+                <div class="table-scrollable">
+                    <table class="table table-condensed table-hover">
+                        <thead>
+                            <tr>
+                                <th> No </th>
+                                <th> Nama Obat </th>
+                                <th> Tanggal Expired </th>
+                                <th> Jumlah Stock </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td> 1 </td>
+                                <td> Mark </td>
+                                <td> makr124 </td>
+                                <td>
+                                    <span class="label label-sm label-success"> Approved </span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td> 2 </td>
+                                <td> Jacob </td>
+                                <td> jac123 </td>
+                                <td>
+                                    <span class="label label-sm label-info"> Pending </span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td> 3 </td>
+                                <td> Cooper </td>
+                                <td> lar </td>
+                                <td>
+                                    <span class="label label-sm label-warning"> Suspended </span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td> 4 </td>
+                                <td> Sandy </td>
+                                <td> sanlim </td>
+                                <td>
+                                    <span class="label label-sm label-danger"> Blocked </span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td> 5 </td>
+                                <td> Lim </td>
+                                <td> sanlim </td>
+                                <td>
+                                    <span class="label label-sm label-danger"> Blocked </span>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <!-- END CONDENSED TABLE PORTLET-->
+    </div>
+    <div class="col-md-6">
+    <!-- BEGIN CONDENSED TABLE PORTLET-->
+    <div class="portlet box green">
+        <div class="portlet-title">
+            <div class="caption">
+                <i class="fa fa-picture"></i>Obat Habis </div>
+                <div class="tools">
+                    <a href="javascript:;" class="collapse" data-original-title="" title=""> </a>
+                    <a href="#portlet-config" data-toggle="modal" class="config" data-original-title="" title=""> </a>
+                    <a href="javascript:;" class="reload" data-original-title="" title=""> </a>
+                    <a href="javascript:;" class="remove" data-original-title="" title=""> </a>
+                </div>
+            </div>
+            <div class="portlet-body">
+                <div class="table-scrollable">
+                    <table class="table table-condensed table-hover">
+                        <thead>
+                            <tr>
+                                <th> No </th>
+                                <th> Nama Obat </th>
+                                <th> Nama Supplier </th>
+                                <th> Status </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td> 1 </td>
+                                <td> Mark </td>
+                                <td> makr124 </td>
+                                <td>
+                                    <span class="label label-sm label-success"> Approved </span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td> 2 </td>
+                                <td> Jacob </td>
+                                <td> Nilson </td>
+                                <td>
+                                    <span class="label label-sm label-info"> Pending </span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td> 3 </td>
+                                <td> Larry </td>
+                                <td> Cooper </td>
+                                <td>
+                                    <span class="label label-sm label-warning"> Suspended </span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td> 4 </td>
+                                <td> Sandy </td>
+                                <td> sanlim </td>
+                                <td>
+                                    <span class="label label-sm label-danger"> Blocked </span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td> 5 </td>
+                                <td> Sandy </td>
+                                <td> sanlim </td>
+                                <td>
+                                    <span class="label label-sm label-danger"> Blocked </span>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <!-- END CONDENSED TABLE PORTLET-->
+    </div>
+    <div class="clearfix"></div>
