89e5fb01789051c4b90eb48a9e771f5d211ae4d3
bangphe <bangphe90@gmail.com>
2016-02-10 17:17:10 +0700

    UPDATE 10/02/2016
    - fixing halaman view order

--- protected/modules/admin/views/order/view.php
+++ protected/modules/admin/views/order/view.php
@@ -6,7 +6,10 @@ $this->breadcrumbs=array(
 	'Data Order'=>array('index'),
 	$model->KODE_ORDER,
 );
-
+$totalItem = 0;
+foreach ($model->orderdetail as $i => $detail) {
+    $totalItem += $detail->JUMLAH;
+}
 ?>
 
 <?php echo Yii::app()->user->getFlash('info') ?>
@@ -18,9 +21,7 @@ $this->breadcrumbs=array(
 				<i class="fa fa-desktop"></i>Data Order #<?php echo $model->KODE_ORDER; ?>
 			</div>
 			<div class="actions">
-				<a href="update" class="btn btn-default btn-sm">
-					<i class="fa fa-pencil"></i> Ubah </a>
-					<?php echo CHtml::link('<i class="fa fa-edit"></i>', array('update', 'id' => $model->KODE_ORDER)) ?>
+				<?php echo CHtml::link('<i class="fa fa-pencil"></i> Ubah', array('update', 'id' => $model->KODE_ORDER), array('class'=>'btn btn-default btn-sm')); ?>
 			</div>
 		</div>
 		<div class="portlet-body">
@@ -32,8 +33,12 @@ $this->breadcrumbs=array(
 						),
 					'attributes'=>array(
 						'KODE_ORDER',
-						'ID_PASIEN',
-						'TANGGAL_ORDER',
+						'pasien.NAMA_PASIEN',
+						array(
+                            'name'=>'TANGGAL_ORDER',
+                            'type'=>'tanggalWaktu',
+                            'value'=>$model->TANGGAL_ORDER,
+                        ),
 						'USER_PEMBUAT',
 						'PEMBAYARAN',
 						'KEMBALIAN',
@@ -57,7 +62,7 @@ $this->breadcrumbs=array(
 					<div class="row">
 						<div class="col-md-4">
 							<label class="control-label">Total</label>
-							<p class="lead">Rp. 67.500</p>
+							<p class="lead"><strong><?php echo MyFormatter::formatUang($model->getTotal()) ?></strong></p>
 						</div>
 						<div class="col-md-4">
 							<div class="form-group">
@@ -80,7 +85,10 @@ $this->breadcrumbs=array(
 					</div>
 				</div>
 				<div class="form-actions center">
-					<a class="btn red pull-right print" id="cetaknota" href="/laundry/order/cetak/000355">Hitung &amp; Cetak Nota</a>
+					<?php echo CHtml::link('Hitung & Cetak Nota', array('/order/cetak', 'id' => $model->KODE_ORDER), array(
+                        'class' => 'btn btn-success pull-right print',
+                        'id' => 'cetaknota'
+                    )) ?>
 				</div>
 			</form>
 				<!-- END FORM-->
@@ -97,14 +105,38 @@ $this->breadcrumbs=array(
             <div class="tools"></div>
         </div>
         <div class="portlet-body">
-            <br>
-            <div class="table-scrollable">
+            <div class="table-responsive">
+               <table class="table table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Nama Barang</th>
+                            <th>Kategori</th>
+                            <th>Jumlah</th>
+                            <th>Harga</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                        $cek = OrderDetail::model()->findAllByAttributes(array('KODE_ORDER' => $model->KODE_ORDER));
+                        foreach ($cek as $dex => $row) {
+                    ?>
+                        <tr>
+                            <td><?php echo $dex+1;?></td>
+                            <td><?php echo $row->item->NAMA_ITEM;?></td>
+                            <td><?php echo $row->item->kategori->KATEGORI;?></td>
+                            <td><?php echo $row->JUMLAH;?></td>
+                            <td><?php echo MyFormatter::formatUang($row->HARGA);?></td>
+                        </tr>
+                    <?php } ?>
+                    </tbody>
+                </table>
+            </div>
+            <div class="well">
                 <table class="table table-condensed table-bordered">
-                    <tbody><tr><th>Subtotal</th><td>Rp. 45.000</td></tr>
-                    <tr><th>Diskon</th><td>0%</td></tr>
-                    <tr><th>Total Item</th><td>1</td></tr>
-                    <tr><th>Biaya Tambahan</th><td>Rp. 22.500</td></tr>
-                    <tr><th>Total</th><td>Rp. 67.500</td></tr>
+                    <tbody>
+                    <tr><th>Total Item</th><td><span class="label label-danger"><?php echo $totalItem;?></span></td></tr>
+                    <tr><th>Total</th><td><strong><?php echo MyFormatter::formatUang($model->getTotal()) ?></strong></td></tr>
                 </tbody></table>
             </div>
         </div>
@@ -113,3 +145,18 @@ $this->breadcrumbs=array(
 </div>
 
 <div class="clearfix"></div>
+
+<script src="<?php echo Yii::app()->theme->baseUrl ?>/assets/custom/jquery.printPage.js" type="text/javascript"></script>
+<script>
+    $(".print").printPage();
+    
+    $('#cetaknota').click(function() {
+        var bayar = $('#bayar').val();
+        bayar = bayar.replace('.', '');
+        
+        var kembali = bayar - <?php echo $model->getTotal() ?>;
+        $('#kembali').attr('value', kembali);
+        
+        $(this).attr('href', $(this).attr('href') + '?bayar=' + bayar + '&kembali=' + kembali);
+    });
+</script>
