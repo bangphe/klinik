35ad81bff1493a7723c48b75c70a4e5371723bd7
bangucen <Moh Husen>
2016-02-11 10:19:45 +0700

    update

--- protected/config/database.php
+++ protected/config/database.php
@@ -5,7 +5,7 @@ return array(
 	'connectionString' => 'mysql:host=localhost;dbname=klinik_db_090216',
 	'emulatePrepare' => true,
 	'username' => 'root',
-	'password' => 'root',
+	'password' => 'sepanjang',
 	'charset' => 'utf8',
 	
 );
