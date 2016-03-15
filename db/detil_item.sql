e4bed63d4b84e27cf62fad747ac9603b752f1a46
bangucen <Moh Husen>
2016-03-15 15:30:36 +0700

    pulvus

--- db/detil_item.sql
+++ db/detil_item.sql
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `klinik_db_110316_empty`
+-- Database: `klinik_db_kosong`
 --
 
 -- --------------------------------------------------------
